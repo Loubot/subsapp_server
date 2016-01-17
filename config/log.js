451ec9cb55cb295953b1a8e5a5f0e3a9287ddf0b
Louis <lllouis@yahoo.com>
2016-01-17 14:39:59 +0000

    Try papertrai

--- config/log.js
+++ config/log.js
@@ -1,16 +1,17 @@
 var winston = require('winston');
 
-var customLogger = new winston.Logger({
+require('winston-papertrail').Papertrail;
+
+  var logger = new winston.Logger({
     transports: [
-        new(winston.transports.File)({
-            level: 'silly',
-            filename: "logfile.log"
-            // filename: "C:\\Users\\angell\\Documents\\sails\\subsapp_server\\logfile.log"
-        }),
-    ],
-});
+        new winston.transports.Papertrail({
+            host: 'logs.papertrailapp.com',
+            port: 12345
+        })
+    ]
+  });
 
 module.exports.log = {
     colors: false,  // To get clean logs without prefixes or color codings
-    custom: customLogger
+    custom: logger
 };
