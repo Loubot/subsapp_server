c6b61775994b26a1f7e6cf62b4649dbd5153077c
Loubot <lllouis@yahoo.com>
2016-01-29 13:55:10 +0000

    Different coffee compiler

--- tasks/config/coffee.js
+++ tasks/config/coffee.js
@@ -1,38 +1,21 @@
-/**
- * Compile CoffeeScript files to JavaScript.
- *
- * ---------------------------------------------------------------
- *
- * Compiles coffeeScript files from `assest/js` into Javascript and places them into
- * `.tmp/public/js` directory.
- *
- * For usage docs see:
- * 		https://github.com/gruntjs/grunt-contrib-coffee
- */
 module.exports = function(grunt) {
-
-	grunt.config.set('coffee', {
-		dev: {
-			options: {
-				bare: true,
-				sourceMap: true,
-				sourceRoot: './'
-			},
-			files: [{
-				expand: true,
-				cwd: 'assets/js/',
-				src: ['**/*.coffee'],
-				dest: '.tmp/public/js/',
-				ext: '.js'
-			}, {
-				expand: true,
-				cwd: 'assets/js/',
-				src: ['**/*.coffee'],
-				dest: '.tmp/public/js/',
-				ext: '.js'
-			}]
-		}
-	});
-
-	grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    coffee: {
+      coffee_to_js: {
+        options: {
+          bare: true,
+          sourceMap: true
+        },
+        expand: true,
+        flatten: false,
+        cwd: "assets",
+        src: ["**/*.coffee"],
+        dest: 'assets',
+        ext: ".js"
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  return grunt.registerTask('compile', ['coffee']);
 };
