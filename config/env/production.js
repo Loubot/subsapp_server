b10afb6a339609b16d68499d5a89eb9ecab33537
Louis <lllouis@yahoo.com>
2016-02-04 15:51:15 +0000

    Change db

--- config/env/production.js
+++ config/env/production.js
@@ -18,7 +18,8 @@ module.exports = {
    ***************************************************************************/
 
   models: {
-    connection: 'subsapp_db_prod'
+    // connection: 'subsapp_db_prod'
+    connection: 'heroku_postgres'
   }
 
   /***************************************************************************
